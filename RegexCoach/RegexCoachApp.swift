4cd42be8f57219f757cfe6ca6368d3a440aa7def
Alexander Ostrovsky <thealeos@gmail.com>
2023-03-21 12:45:01 +0400

    Add the Xcode project

---
+++ RegexCoach/RegexCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegexCoachApp.swift
+//  RegexCoach
+//
+//  Created by Alexander Ostrovsky on 21/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct RegexCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
